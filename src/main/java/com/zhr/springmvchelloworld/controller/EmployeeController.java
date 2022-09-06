a2f66698088da4a7699d443d9e34f7858d8453b7
zhanghaoran <2017929959@qq.com>
2022-09-06 10:08:01 +0800

    ""

--- src/main/java/com/zhr/springmvchelloworld/controller/EmployeeController.java
+++ src/main/java/com/zhr/springmvchelloworld/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.zhr.springmvchelloworld.controller;
 import com.zhr.springmvchelloworld.Employee;
 import com.zhr.springmvchelloworld.dao.employeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +26,19 @@ public class EmployeeController {
         model.addAttribute("allemployee",allEmployee);
         // 跳转到列表页面
         return "employee_list";
+    }
+    @RequestMapping (value = "/employee",method = RequestMethod.POST)
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        // 重定向到列表功能
+        return "redirect:/employee";
+    }
+    @RequestMapping(value = "/employee{id}",method = RequestMethod.GET)
+    public String toUpdate(@PathVariable("id") Integer id,Model model) {
+        Employee employee = employeeDao.get(id);
+        // 将员工共享到请求域中
+        model.addAttribute("employee",employee);
+        return "employee_update";
     }
 
 
